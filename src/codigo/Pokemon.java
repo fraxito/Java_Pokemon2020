f6eecf5306b7626821fbad903300fd8167d4ce6c
fraxito <fraxito@gmail.com>
2020-02-19 19:56:27 +0100

    Con la versión de Hashmap funcionando

---
+++ src/codigo/Pokemon.java
@@ -0,0 +1,16 @@
+package codigo;
+
+/**
+ *
+ * @author Jorge Cisneros
+ */
+public class Pokemon {
+    public String nombre = "";
+    public String especie = "";
+    public String movimiento1 = "";
+    public String peso = "";
+    public int preEvolucion = 0;
+    public int posEvolucion = 0;
+    
+    
+}
--- src/codigo/VentanaPokedex.java
+++ src/codigo/VentanaPokedex.java
@@ -12,6 +12,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.imageio.ImageIO;
@@ -24,11 +25,15 @@ public class VentanaPokedex extends javax.swing.JFrame {
 
     BufferedImage buffer1 = null;
     Image imagen1 = null;
-    int contador = 0;
+    int contador = 1;
     
     Statement estado;
     ResultSet resultadoConsulta;
     Connection conexion;
+    
+    //estructura para guardar todo el contenido de la base de datos
+    //de golpe
+    HashMap<String, Pokemon> listaPokemons = new HashMap();
     
     
     @Override
@@ -65,6 +70,24 @@ public class VentanaPokedex extends javax.swing.JFrame {
                             "root",
                             "");
             estado = conexion.createStatement();
+            resultadoConsulta = estado.executeQuery("Select * from pokemon");
+            //recorremos el array del resultado uno a uno
+            //para ir cargándolo en el Hashmap
+            
+            while (resultadoConsulta.next()){
+                Pokemon p = new Pokemon();
+                p.nombre = resultadoConsulta.getString("nombre");
+                p.especie = resultadoConsulta.getString("especie");
+                p.movimiento1 = resultadoConsulta.getString("movimiento1");
+                p.peso = resultadoConsulta.getString("peso");
+                p.preEvolucion = resultadoConsulta.getInt("preEvolucion");
+                p.posEvolucion = resultadoConsulta.getInt("posEvolucion");
+                
+                //añado el pokemon recien creado al Hashmap
+                listaPokemons.put(resultadoConsulta.getString("id"), p);
+            }
+            
+            
         }
         catch (Exception e){
             System.out.println(e.getMessage());
@@ -174,26 +197,30 @@ public class VentanaPokedex extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void izqActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_izqActionPerformed
+        dibujaElPokemonQueEstaEnLaPosicion(contador);//porque en el png los pokemon empiezan en el 0
+        Pokemon p = listaPokemons.get(String.valueOf(contador+1));  //y en la BBDD en el 1
+        if (p != null){
+            nombrePokemon.setText(p.nombre);
+        }
+        else{
+            nombrePokemon.setText("NO HAY DATOS");
+        }
         contador --;
         if (contador <=0){
-            contador = 1;
+            contador = 0;
         }
-        dibujaElPokemonQueEstaEnLaPosicion(contador);
+
     }//GEN-LAST:event_izqActionPerformed
 
     private void derActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_derActionPerformed
 
         dibujaElPokemonQueEstaEnLaPosicion(contador);
-        
-        try {
-            resultadoConsulta = estado.executeQuery("select * from pokemon where id=" + (contador+1));
-            if (resultadoConsulta.next()){
-                nombrePokemon.setText(resultadoConsulta.getString(2));
-            }
-            else{
-                nombrePokemon.setText("Este pokemon no figura en la pokedex");
-            }
-        } catch (SQLException ex) {
+        Pokemon p = listaPokemons.get(String.valueOf(contador+1));
+        if (p != null){
+            nombrePokemon.setText(p.nombre);
+        }
+        else{
+            nombrePokemon.setText("NO HAY DATOS");
         }
         contador ++;
         if (contador >=649){
